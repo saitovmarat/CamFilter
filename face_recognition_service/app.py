93117dcf49f689b655844f473ad1bf8b46440b63
saitovmarat <maratsajtov@gmail.com>
2025-03-31 10:17:09 +0300

    [add] name output

--- face_recognition_service/app.py
+++ face_recognition_service/app.py
@@ -5,9 +5,16 @@ import face_recognition
 
 app = Flask(__name__)
 
+known_face_encodings = [
+    face_recognition.face_encodings(face_recognition.load_image_file("../resources/people/1.jpg"))[0],
+]
+
+known_face_names = [
+    "Marat Saitov",
+]
+
 @app.route('/upload', methods=['POST'])
 def upload_image():
-    
     if 'file' not in request.files:
         return jsonify({'error': 'No file part'}), 400
 
@@ -18,10 +25,27 @@ def upload_image():
     in_memory_file = np.frombuffer(file.read(), np.uint8)
     img = cv2.imdecode(in_memory_file, cv2.IMREAD_COLOR)
     rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    face_locations = face_recognition.face_locations(rgb_img)
 
-    for (top, right, bottom, left) in face_locations:
+    face_locations = face_recognition.face_locations(rgb_img)
+    face_encodings = face_recognition.face_encodings(rgb_img, face_locations)
+
+    face_names = []
+    for face_encoding in face_encodings:
+        matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+        name = "Unknown"
+
+        if True in matches:
+            first_match_index = matches.index(True)
+            name = known_face_names[first_match_index]
+
+        face_names.append(name)
+
+    for (top, right, bottom, left), name in zip(face_locations, face_names):
         cv2.rectangle(img, (left, top), (right, bottom), (0, 255, 0), 2)
+
+        cv2.rectangle(img, (left, bottom - 35), (right, bottom), (0, 255, 0), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(img, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
 
     _, img_encoded = cv2.imencode('.jpg', img)
     response = img_encoded.tobytes()
